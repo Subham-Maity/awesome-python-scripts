42656af621935d16c6508f417d1171045a996070
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-03-24 06:26:08 +0530

    Update main.py

--- JSON Data into a Tree image/main.py
+++ JSON Data into a Tree image/main.py
@@ -1,10 +1,6 @@
 import json
 from graphviz import Digraph
 
-# Load JSON data from file
-with open('package-lock.json', 'r') as f:
-    data = json.load(f)
-
 # Create a new directed graph
 dot = Digraph()
 
@@ -13,18 +9,27 @@ def add_nodes_edges(data, parent=None):
     if isinstance(data, dict):
         for key, value in data.items():
             dot.node(key)
-            if parent:
+            if parent is not None:
                 dot.edge(parent, key)
             add_nodes_edges(value, key)
     elif isinstance(data, list):
         for i, item in enumerate(data):
             node_id = f'{parent}_{i}'
             dot.node(node_id, str(item))
-            dot.edge(parent, node_id)
+            if parent is not None:
+                dot.edge(parent, node_id)
             add_nodes_edges(item, node_id)
 
-# Call the recursive function on the JSON data
-add_nodes_edges(data)
+def create_tree(data):
+    # Call the recursive function on the JSON data
+    add_nodes_edges(data)
 
-# Save the generated image to a file
-dot.render('tree.gv', view=True)
+    # Save the generated image to a file
+    dot.render('tree.gv', view=True)
+
+# Load JSON data from file
+with open('example.json', 'r') as f:
+    data = json.load(f)
+
+# Call the create_tree function on the JSON data
+create_tree(data)
